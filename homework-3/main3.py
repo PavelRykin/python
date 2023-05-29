cfe4184bc6207d8292f646f0c43281cf272d024d
Pavel <djassoniek@gmail.com>
2023-05-29 03:52:39 +0300

    add task-18

--- homework-3/main3.py
+++ homework-3/main3.py
@@ -14,7 +14,15 @@ list_number = []
 
 print("Вот полученный массив от о до ", number)
 
-for i in range(number + 1):
+for i in range(1, number + 1):
     list_number.append(i)
 
 print(*list_number)
+
+number2 = int(input("Введите число: "))
+result = 0
+if number2 > len(list_number):
+    result = len(list_number)
+elif number2 <= len(list_number):
+    result = number2 - 1
+print(result)
